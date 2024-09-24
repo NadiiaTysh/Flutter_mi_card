769e564b2e5c5e4de21f771ad5889ac87f824727
NadiiaTyshko <syao.xiwang@gmail.com>
2024-09-24 13:15:04 -0500

    Business card

--- lib/main.dart
+++ lib/main.dart
@@ -13,34 +13,74 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
             children: <Widget>[
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.red,
+              const CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/cat.jpg'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.green,
-                  ),
-                ],
+              const Text(
+                'Moon Night',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              const Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    color: Colors.tealAccent,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    letterSpacing: 2.5),
               ),
               Container(
-                height: 100,
-                width: 100,
-                color: Colors.blue,
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '123-456-7890',
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 20,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'moon@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 20,
+                      ),
+                    )
+                  ],
+                ),
               ),
             ],
           ),
